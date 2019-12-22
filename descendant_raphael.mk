bed0a7da2b31d92382a91586d3e33aaf987faee5
jhenrique09 <jhsv09@gmail.com>
2019-12-22 08:10:44 +0000

    raphael: Build customized version of ANX Camera

--- descendant_raphael.mk
+++ descendant_raphael.mk
@@ -22,3 +22,6 @@ PRODUCT_NAME := descendant_raphael
 BUILD_FINGERPRINT := "Xiaomi/raphaelin/raphaelin:9/PKQ1.181121.001/V10.3.3.0.PFKINXM:user/release-keys"
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+# Inherit from ANXCamera vendor.
+$(call inherit-product, vendor/ANXCamera/config.mk)
